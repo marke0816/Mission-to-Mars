d90bd808223a6befeee5c78ef9cde3e967dc43ed
Mark Esposito <markanthonyesposito@hotmail.com>
2021-04-22 12:38:17 -0500

    10.5.3

--- scraping.py
+++ scraping.py
@@ -3,9 +3,29 @@ from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
+
+# executable_path = {'executable_path': ChromeDriverManager().install()}
+# browser = Browser('chrome', **executable_path, headless=False)
 
 def mars_news(browser):
 
@@ -81,4 +101,8 @@ def mars_facts():
     return df.to_html()
 
 # End the scraping session
-browser.quit()
+# browser.quit()
+
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
